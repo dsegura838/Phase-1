0af4c98bcff00289fef941a2c81f73b98a7f6e49
David Segura <davidsegura838@gmail.com>
2021-02-17 09:15:17 -0600

    corrected application

--- ThreadExample/src/ThreadExample/LongestIncreasingApplication.java
+++ ThreadExample/src/ThreadExample/LongestIncreasingApplication.java
@@ -1,6 +1,7 @@
 package ThreadExample;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Random;
 import java.util.concurrent.ThreadLocalRandom;
@@ -14,65 +15,48 @@ public class LongestIncreasingApplication {
 		SequenceGenerator sg = new SequenceGenerator(n);
 		ArrayList<Integer> sequence = sg.GenerateSequence();
 		
-		//create lowest variable
-		int lowest = 0;
-		//create highest variable
-		int highest = 0;
-		
-		int increasing = 0;
-		int decreasing = 0;
-		
-		//create arraylist
-		List<Integer> sub_seq = new ArrayList<>();
-		int tempLongestSeq =0;
+		//Create Counters		
+		int tempLongestSeq =1;
 		int longestSeq = 0;
-		boolean running = false;
-		int start=0;
-		int end=0;
 		
+		//Print out the sequence
+		System.out.println(sequence);
 		
-		for (int i : sequence){
+		//loop through arraylist
+		for(int i = 0; i<= sequence.size()-2; i++) {			
 			
-			start =0;
-			end = 0;
-			
-			if(i+1 < sequence.size()-1) {
-				//lowest = ;
-				//highest = ;
+			//compare values
+			if(sequence.get(i) < sequence.get(i+1)) {
 				
-				if(sequence.get(i) <= sequence.get(i+1)) {
-					//lowest = sequence.get(i);
-					//highest = sequence.get(i+1);
-					tempLongestSeq++;
+				//autoincrement temporary
+				tempLongestSeq++;
+				
+				//print out the temporary longest sequence
+				System.out.println(tempLongestSeq);
+				
+				//compare temporary longest sequence with longest sequence
+				if(tempLongestSeq > longestSeq) {
 					
-					//System.out.println(lowest+", "+highest);
-					if(!running) {
-						lowest = sequence.indexOf(i);
-						running = true;	
-					}
-									
-					
-				}
-				else if(running && (tempLongestSeq > longestSeq)){
-					System.out.println("Starting new sequence");
+					//if true, assign temporary value to longest value
 					longestSeq = tempLongestSeq;
-					tempLongestSeq = 0;
-					
-					decreasing++;
-					running = false;
-					System.out.println(running);
-					highest = sequence.indexOf(i);
-					//sub_seq.clear();
 				}
+				
+				
+			}
+			else {
+				System.out.println("False");
+				
+				//restart temporary sequence
+				tempLongestSeq = 1;
 			}
 			
+			
 		}
-		//System.out.println("Increasing: "+ increasing);
-		//System.out.println("Decreasing: "+ decreasing);
-		//System.out.println(start);
-		//System.out.println(end);
-		System.out.println(tempLongestSeq);
-		System.out.println(sequence);
-		System.out.println(sequence.subList(lowest, highest));
+		//Print out the longest subsequence
+		System.out.println();
+		System.out.println("The longest subsequence is: "+longestSeq);
 	}
+			
 }
+
+
